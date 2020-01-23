fff7aa52e05d6ab0e5610fd870343f761d7b6385
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-01-23 01:30:25 +0100

    navbar setup and routes done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import { Container } from 'semantic-ui-react';
+
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import MenuBar from './components/MenuBar';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome</h1>
-    </div>
+    <Router>
+      <div className='ui container'>
+        <MenuBar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+      </div>
+    </Router>
   );
 }
 
