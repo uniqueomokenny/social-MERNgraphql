7919c1f2595c2e6bcff1ba17fa130cf0eeacabc6
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-01-25 01:08:26 +0100

    register and login done with custom useForm hooks

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form, Header } from 'semantic-ui-react';
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
-function Login() {
+import { useForm } from '../util/hooks';
+
+function Login({ history }) {
+  const [errors, setErrors] = useState({});
+
+  const { onChange, onSubmit, values } = useForm(loginUserCallback, {
+    username: '',
+    password: ''
+  });
+
+  const [login, { loading }] = useMutation(LOGIN_USER, {
+    update(_, result) {
+      history.push('/');
+    },
+    onError(err) {
+      setErrors(err.graphQLErrors[0].extensions.exception.errors);
+    },
+    variables: values
+  });
+
+  function loginUserCallback() {
+    login();
+  }
+
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className='form-container'>
+      <Form onSubmit={onSubmit} noValidate className={loading ? 'loading' : ''}>
+        <Header as='h1'>Login</Header>
+        <Form.Input
+          label='Username'
+          placeholder='Username..'
+          name='username'
+          type='text'
+          value={values.username}
+          error={errors.username ? true : false}
+          onChange={onChange}
+        />
+
+        <Form.Input
+          label='Password'
+          placeholder='Password..'
+          name='password'
+          type='password'
+          value={values.password}
+          error={errors.password ? true : false}
+          onChange={onChange}
+        />
+
+        <Button type='submit' primary>
+          Login
+        </Button>
+      </Form>
+      {Object.keys(errors).length > 0 && (
+        <div className='ui error message'>
+          <ul className='list'>
+            {Object.values(errors).map(value => (
+              <li key={value}>{value}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
+
+const LOGIN_USER = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      id
+      email
+      username
+      createdAt
+      token
+    }
+  }
+`;
 
 export default Login;
--- utils/validators.js
+++ utils/validators.js
@@ -6,12 +6,12 @@ module.exports.validateRegisterInput = (
 ) => {
   const errors = {};
   if (username.trim() === '') errors.username = 'Username must not be empty';
-  if (email.trim() === '') errors.username = 'Email must not be empty';
+  if (email.trim() === '') errors.email = 'Email must not be empty';
   else {
     const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!email.match(regEx)) errors.email = 'Email must be valid email address';
   }
-  if (password === '') errors.username = 'Password must not be empty';
+  if (password === '') errors.password = 'Password must not be empty';
   else if (password !== confirmPassword)
     errors.confirmPassword = 'Passwords must match';
 
